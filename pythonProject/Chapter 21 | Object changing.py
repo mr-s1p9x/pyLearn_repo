284a92a3fdfa7c87c0b6e24e8b7da280c7b9fa7d
s1pex <temazex777@gmail.com>
2025-01-07 22:44:57 +0200

    chapter 21 | adding some info

--- pythonProject/Chapter 21 | Object changing.py
+++ pythonProject/Chapter 21 | Object changing.py
@@ -8,4 +8,21 @@ print(id(other_number)) # 125168704681200
 
 print(id(10)) # 125168704681200
 
+# Some practice
+from copy import deepcopy
+
+info = {
+    'name': 'Bogdan',
+    'is_instructor': True,
+    'reviews': []
+}
+
+info_deepcopy = deepcopy(info)
+
+info_deepcopy['reviews'].append('Great course!')
+info['reviews'].append('Super')
+
+print(info_deepcopy)
+print(info)
+
 
