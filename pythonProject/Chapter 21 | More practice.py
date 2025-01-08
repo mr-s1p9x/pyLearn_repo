347421d7d25949b771be546bc57db14387bf6c4f
s1pex <temazex777@gmail.com>
2025-01-08 18:17:22 +0200

    chapter 21 | more practice added

--- pythonProject/Chapter 21 | More practice.py
+++ pythonProject/Chapter 21 | More practice.py
@@ -1,86 +1,81 @@
-## if, for example, string or int has the same value
-# they will refer to one, same address in memory
-# this if for the Python optimization, because those objects immutable
-# For example:
+# Example 1: Immutable objects (e.g., strings and integers)
+# If, for example, a string or an integer has the same value,
+# they will refer to the same memory address in Python.
+# This is due to Python's optimization for immutable objects.
+
+from copy import deepcopy
+
 number_1 = 10
 number_2 = 10
 
-print(id(number_1)) # f.e. 130876185638128
-print(id(number_2)) # the same as above
+print(id(number_1))  # e.g., 130876185638128
+print(id(number_2))  # Same as above because integers are immutable
 
 my_string_1 = 'Hello Python'
 my_string_2 = 'Hello Python'
 
-print(id(my_string_1)) # f.e. 127340959725680
-print(id(my_string_2)) # the same as above
-# those objects using one link to refer one address in memory
-###############
+print(id(my_string_1))  # e.g., 127340959725680
+print(id(my_string_2))  # Same as above because strings are immutable
+# Immutable objects with the same value share the same memory address
+
 print("========")
 
-# more examples
+# Example 2: Mutable objects (e.g., lists)
+# Mutable objects always have different memory addresses even if their values are the same
 first_num = 1023
 second_num = first_num
 
-print(id(first_num)) # f.e. 129479243578000
-print(id(second_num)) # the same as above
+print(id(first_num))  # e.g., 129479243578000
+print(id(second_num))  # Same as above because they reference the same integer
 
 second_num += 5
 
-print(id(first_num)) # f.e. 129479243578000
-print(id(second_num)) # different, f.e. 125358040064336
+print(id(first_num))  # Still the same as before
+print(id(second_num))  # Different because a new integer was created for "second_num"
 
-#### Addresses of mutable objects
 print("========")
 print("Addresses of mutable objects")
 my_list = [1, 2, 3]
-print(id(my_list)) # one id
+print(id(my_list))  # Memory address of the first list
 
 other_list = [1, 2, 3]
-print(id(other_list)) # different from above id
+print(id(other_list))  # Different address because lists are mutable
 
-print(id([1, 2, 3])) # one more different id
-# this happens because lists are mutable objects
-# and every list creation will be assigned different address in memory
-# even if objects in the lists are the same - Python will create
-# new link (address in memory)
+print(id([1, 2, 3]))  # Another different address for a newly created list
+# Lists are mutable, and every new list creation results in a unique memory address
 
 print("========")
-# dictionary examples
+# Example 3: Working with dictionaries
 info = {
     'name': 'Bogdan',
     'is_instructor': True
 }
 
-copy_info = info
+copy_info = info  # Shallow copy (direct reference)
 
-print("Printing both dictionaries (one was copied from another")
+print("Printing both dictionaries (one was copied from another)")
 print(info)
 print(copy_info)
 
-# Now add one pair into copy_info dict and print both
+# Adding a new key-value pair to "copy_info" affects both dictionaries
 copy_info['rev_qty'] = 5
 
 print("Printing both dictionaries after adding one pair into 'copy_info'")
 print(info)
 print(copy_info)
-# and see that both dictionaries have 'rev_qty' string
 
-print()
+# Modifying a shared reference impacts both objects
 info['rev_qty'] = 100
-# now both have 100
 print(info)
 print(copy_info)
 
-# are they equal each other ?
-print(info == copy_info) # True
-# are they have the same id ?
-print(id(info) == id(copy_info)) # True
+# Are they equal in content?
+print(info == copy_info)  # True
+# Do they have the same memory address?
+print(id(info) == id(copy_info))  # True
 
 print("========")
-# But what if we need create different objects?
-# we can just create separate objects with the same key-pairs
-# but they will be different
-
+# Creating independent dictionaries
 info_1 = {
     'name': 'Stacy',
     'is_journalist': True,
@@ -93,9 +88,80 @@ info_2 = {
     'city_to_live': 'New York'
 }
 
-# now lets see, if those objects are differ
-print(id(info_1)) # One id
-print(id(info_2)) # differ from above id
+# Both dictionaries have the same content but different memory addresses
+print(id(info_1))  # One memory address
+print(id(info_2))  # Different memory address
+print(id(info_1) == id(info_2))  # False | Different objects
 
-print(id(info_1) == id(info_2)) # False | different objects
+print("========")
+# Example 4: Avoiding shared references
+superhero = {
+    'name': 'Peter',
+    'superhero-name': 'Spider-Man',
+    'place_to_live': 'New York'
+}
 
+# Shallow copy (direct reference to inner objects)
+copy_superhero = superhero.copy()
+
+# Adding a new key-value pair only affects "copy_superhero"
+copy_superhero['suit'] = 'Spider-Man | 2077'
+
+print(superhero)
+print(copy_superhero)
+
+# Different dictionary IDs
+print(id(superhero))
+print(id(copy_superhero))
+
+print()
+# Example 5: Shared references in nested structures
+superhero_2 = {
+    'name': 'Logan',
+    'superhero-name': 'Wolverine',
+    'place_to_live': 'Xavier University, Alberta, CA',
+    'cite': []  # Nested mutable object
+}
+
+copy_superhero_2 = superhero_2.copy()
+copy_superhero_2['cite'].append(
+    "My whole life I felt like an animal. "
+    "I ignored my instincts and I ignored what I really am."
+)
+
+# Both dictionaries share the same reference to the "cite" list
+print(copy_superhero_2)
+print(superhero_2)
+
+# Different dictionary IDs but shared references for nested objects
+print(id(copy_superhero_2))
+print(id(superhero_2))
+
+# Changing other keys won't affect the original
+superhero_2['name'] = 'Spider'
+print(copy_superhero_2)
+print(superhero_2)
+
+print()
+# Example 6: Using deepcopy to avoid shared references
+superhero_3 = {
+    'name': 'Wade',
+    'superhero-name': 'Deadpool',
+    'city_to_live': 'New York',
+    'cite': []
+}
+
+copy_superhero_3 = deepcopy(superhero_3)
+
+# Different IDs = independent objects
+print(id(copy_superhero_3))
+print(id(superhero_3))
+
+copy_superhero_3['cite'].append(
+    "Life is an endless series of trainwrecks with only brief, "
+    "commercial-like breaks of happiness"
+)
+
+# "cite" was added only to "copy_superhero_3"
+print(copy_superhero_3)
+print(superhero_3)  # Original remains unchanged
