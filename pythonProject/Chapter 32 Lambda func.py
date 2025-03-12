9460639756f8e2469e469c2db99591993bb9d9aa
s1pex <temazex777@gmail.com>
2025-03-12 15:56:00 +0200

    lambda practice

---
+++ pythonProject/Chapter 32 Lambda func.py
@@ -0,0 +1,19 @@
+# casual func
+# def mult(a, b):
+#     return a * b
+
+# lambda func
+# lambda a, b: a * b
+
+# practice 
+def greeting(greet):
+    return lambda name: f"{greet}, {name}!"
+
+morning_greeting = greeting("Good morning")
+
+print(morning_greeting("Artem"))
+# Good morning, Artem!
+
+evening_greeting = greeting("Good evening")
+
+print(evening_greeting("Artem"))
